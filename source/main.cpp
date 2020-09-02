2ebf4ada370dd7b9fdd8492404dea8500010b90a
LirrK <juliushemme@yahoo.de>
2020-09-02 12:22:34 +0200

    Integrate color.hpp | Change gitignore: ignore all *.ppm files

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <utility>
+#include <cmath>
+#include "../framework/color.hpp"
 
 int main() {
 
@@ -6,6 +11,7 @@ int main() {
 
   const int image_width = 256;
   const int image_height = 256;
+  Color color{0.5f, 0.5f, 0.5f};
 
   // Renderer
 
@@ -13,13 +19,19 @@ int main() {
 
   for(int j = image_height-1; j >= 0; --j) {
     for(int i = 0; i < image_width; ++i) {
-      auto r = double(i) / (image_width-1);
-      auto g = double(j) / (image_height-1);
-      auto b = 0.25;
+      //glm::vec3 col(float(i) / float(image_width), float(j) / float(image_height), 0.2);
+      color.r = float(i) / float(image_width);
+      color.g = float(j) / float(image_height);
+      color.b = 0.2;
 
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
+      /*
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
+      */
+      int ir = int(255.99 * color.r);
+      int ig = int(255.99 * color.g);
+      int ib = int(255.99 * color.b);
 
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
     }
