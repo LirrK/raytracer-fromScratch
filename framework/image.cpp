e379cee176ce0ec0e5191af2e04d31d9a449454d
LirrK <juliushemme@yahoo.de>
2020-09-03 15:53:00 +0200

    Modify image: fix getter and constructors

--- framework/image.cpp
+++ framework/image.cpp
@@ -3,12 +3,16 @@
 Image::Image(){};
 
 Image::Image(int image_width, int image_height)
-      : image_width_(image_width), image_height_(image_height)
-      { aspect_ratio_ = (image_width_ / image_height_); };
+      : image_width_(image_width),
+        image_height_(image_height), 
+        aspect_ratio_(image_width / image_height)
+      {};
   
 Image::Image(int image_width, float aspect_ratio)
-      : image_width_(image_width), aspect_ratio_(aspect_ratio)
-      { image_height_ = int(image_width_ / aspect_ratio_); };
+      : image_width_(image_width), 
+        aspect_ratio_(aspect_ratio), 
+        image_height_(int(image_width / aspect_ratio))
+      {};
 
 
 int Image::get_image_width() { return image_width_; }
--- source/main.cpp
+++ source/main.cpp
@@ -5,30 +5,31 @@
 #include <cmath>
 #include "../framework/color.hpp"
 #include "../framework/ray.hpp"
+#include "../framework/image.hpp"
 
 int main() {
 
   // Image
 
-  const int image_width = 256;
-  const int image_height = 256;
+  Image image{680, 480};
+
   Color color1{0.5f, 0.5f, 0.5f};
 
   // Renderer
 
   // P3 -> ASCII
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+  std::cout << "P3\n" << image.get_image_width() << ' ' << image.get_image_height() << "\n255\n";
 
   // Iterating over every pixel
-  for(int j = image_height-1; j >= 0; --j) {
+  for(int j = image.get_image_height()-1; j >= 0; --j) {
     std::cerr << "\rReihen verbleibend: " << j << ' ' << std::flush;
 
-    for(int i = 0; i < image_width; ++i) {
+    for(int i = 0; i < image.get_image_width(); ++i) {
       
       // calculating a color value for each pixel (first step)
       // ! scheme was choosen for a helloWorld example
-      color1.r = float(i) / float(image_width);
-      color1.g = float(j) / float(image_height);
+      color1.r = float(i) / float(image.get_image_width());
+      color1.g = float(j) / float(image.get_image_height());
       color1.b = 0.2;
 
       // casting color values to int (second step)
